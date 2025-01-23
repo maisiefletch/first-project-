55005d334bc1bdb6722dc2e1bb399d4747ba9dfd
maisiefletch <maisie_fletcher@biswashington.org>
2025-01-23 09:48:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
 with open("students.txt", "w") as file:
  
-    file.write("Lida\n","101\n")
-    file.write("Tara,5\n")
-    file.write("Allessandro,12\n")
-    file.write("Rafael,46\n")
-    file.write("Oli,34\n")
-    file.write("Aaron,21\n")
-    file.write("Jolie,76\n")
-    file.write("Maisie,90\n")
-    file.write("Ollie,65\n")
-    file.write("Harry,46\n")
-    file.write("Ken,83\n")
-    file.write("Bayne,99\n")
-    file.write("Leila,96\n")
-    file.write("Amelia,67\n")
-    file.write("Selma,75\n")
-    file.write("Sofi\n")
-    file.write("Tao\n")
-    file.write("Marlee\n")
-    file.write("Layla\n")
-    file.write("Ben\n")
-    file.write("Letlotlo\n")
-    file.write("Bram\n")
-    file.write("Joseph\n")
-    file.write("Bella\n")
-    file.write("Axel\n")
  
 with open("students.txt", "r") as file:
     for line in file:
